8e29b0cbdcf6b28ac0cf59843265794e12edefdf
flashdato <flashdato@gmail.com>
2024-02-15 00:42:57 +0400

    first commit

---
+++ public/app.js
@@ -0,0 +1,6 @@
+// Inside public/app.js
+document.addEventListener('DOMContentLoaded', () => {
+    const audioPlayer = document.getElementById('audioPlayer');
+    audioPlayer.src = 'audio1.mp3';  // Update to the correct file name
+  });
+  
